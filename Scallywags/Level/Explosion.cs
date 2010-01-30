7c1ed14197d1385492bb5fabd1a7b96923fe8588
Cyril MacIsaac <theoverlordsrealm@gmail.com>
2010-01-30 22:11:49 +0000

    git-svn-id: file:///e/SVN/lamb_mines@37 679230f0-ee8b-9842-8691-e4a0ef3d3c8c

---
+++ Scallywags/Level/Explosion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace Scallywags
+{
+	class Explosion:AnimatedObject
+	{
+
+
+		public Explosion(Vector2 Location, List<Animation> animationList, Texture2D tex)
+            : base(Location, animationList, tex)
+        {
+
+        }
+
+		public override bool Update(float elapsedTime)
+		{
+
+			AnimationPlay.PlayAnimation(Animations[0]);
+
+			base.Update(elapsedTime);
+
+			return isAlive;
+
+		}
+
+		public override void Draw(SpriteBatch sb, GameTime gameTime, Vector2 Offset)
+        {
+            base.Draw(sb, gameTime, Offset);
+        }
+
+		public override void Kill()
+		{
+			KillMe();
+		}
+		public override string WhatAmI()
+		{
+			return "Explosion";
+		}
+	}
+}
